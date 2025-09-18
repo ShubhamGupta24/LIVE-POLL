15fcf1acee5b8d7661ada1ea9c2abf714848e8d1
ShubhamGupta24 <153915903+ShubhamGupta24@users.noreply.github.com>
2025-09-18 13:50:19 +0530

    Adding dot env in backend

--- live-poll/src/api.js
+++ live-poll/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // backend base URL
 const API_URL = process.env.REACT_APP_BACKEND_CONNECT_API + "/api/polls";
-
+console.log("🔗 API URL:", API_URL);
 
 export const hello = () => axios.get(`${API_URL}`);
 
